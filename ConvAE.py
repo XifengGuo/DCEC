e7dd222788af560d2419f5b3255f3adbe202fc5c
Jitka <jitka.polaskova753@gmail.com>
2022-03-28 16:03:40 +0200

    fixed stride size to mach input and expected output shape

--- ConvAE.py
+++ ConvAE.py
@@ -3,29 +3,28 @@ from keras.models import Sequential, Model
 from keras.utils.vis_utils import plot_model
 import numpy as np
 
-
-def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):
+def CAE(input_shape=(1000, 6, 1), filters=[32, 64, 128, 10]):
     model = Sequential()
     if input_shape[0] % 8 == 0:
         pad3 = 'same'
     else:
         pad3 = 'valid'
-    model.add(Conv2D(filters[0], 5, strides=2, padding='same', activation='relu', name='conv1', input_shape=input_shape))
+    model.add(Conv2D(filters[0], 5, strides=(2,5), padding='same', activation='relu', name='conv1', input_shape=input_shape))
 
-    model.add(Conv2D(filters[1], 5, strides=2, padding='same', activation='relu', name='conv2'))
+    model.add(Conv2D(filters[1], 5, strides=(2,1), padding='same', activation='relu', name='conv2'))
 
-    model.add(Conv2D(filters[2], 3, strides=2, padding=pad3, activation='relu', name='conv3'))
+    model.add(Conv2D(filters[2], 3, strides=(2,1), padding=pad3, activation='relu', name='conv3'))
 
     model.add(Flatten())
     model.add(Dense(units=filters[3], name='embedding'))
-    model.add(Dense(units=filters[2]*int(input_shape[0]/8)*int(input_shape[0]/8), activation='relu'))
+    model.add(Dense(units=filters[2]*int(input_shape[0]/8)*int(input_shape[1]/5), activation='relu'))
 
-    model.add(Reshape((int(input_shape[0]/8), int(input_shape[0]/8), filters[2])))
-    model.add(Conv2DTranspose(filters[1], 3, strides=2, padding=pad3, activation='relu', name='deconv3'))
+    model.add(Reshape((int(input_shape[0]/8), int(input_shape[1]/5), filters[2])))
+    model.add(Conv2DTranspose(filters[1], 3, strides=(2,1), padding=pad3, activation='relu', name='deconv3'))
 
-    model.add(Conv2DTranspose(filters[0], 5, strides=2, padding='same', activation='relu', name='deconv2'))
+    model.add(Conv2DTranspose(filters[0], 5, strides=(2,1), padding='same', activation='relu', name='deconv2'))
 
-    model.add(Conv2DTranspose(input_shape[2], 5, strides=2, padding='same', name='deconv1'))
+    model.add(Conv2DTranspose(input_shape[2], 5, strides=(2,5), padding='same', name='deconv1'))
     model.summary()
     return model
 
@@ -35,7 +34,7 @@ if __name__ == "__main__":
     # setting the hyper parameters
     import argparse
     parser = argparse.ArgumentParser(description='train')
-    parser.add_argument('--dataset', default='usps', choices=['mnist', 'usps'])
+    parser.add_argument('--dataset', default='usps', choices=['mnist', 'usps', 'fasta'])
     parser.add_argument('--n_clusters', default=10, type=int)
     parser.add_argument('--batch_size', default=256, type=int)
     parser.add_argument('--epochs', default=200, type=int)
@@ -48,12 +47,14 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # load dataset
-    from datasets import load_mnist, load_usps
+    from datasets import load_mnist, load_usps, load_fasta
     if args.dataset == 'mnist':
         x, y = load_mnist()
     elif args.dataset == 'usps':
         x, y = load_usps('data/usps')
-
+    elif args.dataset == 'fasta':
+        x, y = load_fasta()
+    
     # define the model
     model = CAE(input_shape=x.shape[1:], filters=[32, 64, 128, 10])
     plot_model(model, to_file=args.save_dir + '/%s-pretrain-model.png' % args.dataset, show_shapes=True)
